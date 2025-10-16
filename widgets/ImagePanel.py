f3d685db259a116bd8728b7d0f0fe387451c006b
cristibutica <Butica.Vi.Cristian@student.utcluj.ro>
2025-10-16 11:02:25 +0300

    Add image panel with grayscale conversion and file picker

--- widgets/ImagePanel.py
+++ widgets/ImagePanel.py
@@ -1,5 +1,6 @@
 import os
 from PySide6 import QtWidgets, QtGui, QtCore
+from PySide6.QtWidgets import QFileDialog
 from skimage import io, color
 from skimage.util import img_as_ubyte
 
@@ -7,16 +8,19 @@ class ImagePanel(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
 
-        self.setWindowFilePath("Grayscale Converter")
-        self.original_path = "images/mountains.webp"
-        self.gray_path = "images/mountains_gray.png"  # new file we will create
+        self.gray_path = None
+        self.setWindowTitle("Grayscale Converter")
+        # self.original_path = "images/mountains.webp"
+        # self.gray_path = "images/mountains_gray.png"  # new file we will create
 
+        self.imageUploader = QtWidgets.QPushButton("Choose an image",self)
+        self.imageUploader.clicked.connect(self.pickImage)
         # Load original image
-        pixmap = QtGui.QPixmap(self.original_path)
+        # pixmap = QtGui.QPixmap(self.original_path)
 
         self.imageLabel = QtWidgets.QLabel(self)
-        self.imageLabel.setPixmap(pixmap)
-        self.imageLabel.setScaledContents(True)
+        # self.imageLabel.setPixmap(pixmap)
+        # self.imageLabel.setScaledContents(True)
 
         self.button = QtWidgets.QPushButton("Convert to Grayscale", self)
         self.button.clicked.connect(self.convertImage)
@@ -24,6 +28,7 @@ class ImagePanel(QtWidgets.QWidget):
         layout = QtWidgets.QVBoxLayout(self)
         layout.addWidget(self.imageLabel)
         layout.addWidget(self.button)
+        layout.addWidget(self.imageUploader)
 
     @QtCore.Slot()
     def convertImage(self):
@@ -37,3 +42,22 @@ class ImagePanel(QtWidgets.QWidget):
         # Load the new file into pixmap
         pixmap = QtGui.QPixmap(self.gray_path)
         self.imageLabel.setPixmap(pixmap)
+
+    @QtCore.Slot()
+    def pickImage(self):
+        file_path, _ = QFileDialog.getOpenFileName(
+            self,
+            "Pick an Image",
+            ".",
+            "Images (*.webp *.png *.jpg *.jpeg)"
+        )
+        pixmap = QtGui.QPixmap(file_path)
+        self.imageLabel.setPixmap(pixmap)
+        self.imageLabel.setScaledContents(True)
+
+        # self.gray_path = file_path.split("/")[] + "_gray" + file_path.split(".")[1]
+        # print(self.gray_path)
+
+
+
+
